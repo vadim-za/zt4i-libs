7438fefb660d1341e336d85a85d8cddef844d555
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-07-18 16:56:22 +0200

    Removed currently unsupported list copy methods

--- cc/lists/common.zig
+++ cc/lists/common.zig
@@ -5,42 +5,6 @@ pub fn Methods(Container: type) type {
         const Node = Container.Node;
         const Hook = Container.Hook;
 
-        // TODO:
-        /// Doesn't move ownership, the new list is rather readonly
-        pub fn uncheckedCopyFrom(
-            self: *Container,
-            from: *const Container,
-        ) void {
-            self.* = from.*;
-            if (comptime std.debug.runtime_safety)
-                self.check_ownership = false;
-        }
-
-        /// Moves ownership, the old list is still readable
-        pub fn uncheckedMoveFrom(
-            self: *Container,
-            from: *const Container,
-        ) void {
-            self.* = from.*;
-            if (comptime std.debug.runtime_safety)
-                self.check_ownership = false;
-        }
-
-        /// Moves ownership, the old list is still readable
-        pub fn moveFrom(self: *Container, from: *Container) void {
-            self.* = from.*;
-
-            if (comptime std.debug.runtime_safety) {
-                if (from.check_ownership) {
-                    from.check_ownership = false;
-
-                    var node = self.first();
-                    while (node) |n| : (node = self.next(n))
-                        n.owner = self;
-                }
-            }
-        }
-
         pub fn hookFromFreeNode(self: *const Container, node: *Node) *Hook {
             // Free nodes have undefined hooks, so we cannot check ownership
             return @constCast(self.layout.hookFromNode(node));
