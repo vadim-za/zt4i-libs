8260bdef07eb29d3d10fed97d8f9cd3e0f877d6f
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-07-18 19:34:01 +0200

    List testing insertFirst

--- cc/lists/dbl/testing.zig
+++ cc/lists/dbl/testing.zig
@@ -85,6 +85,49 @@ test "insertLast" {
         })
             try std.testing.expectEqual(&nodes[i], n);
     }
+}
 
-    @breakpoint();
+test "insertFirst" {
+    inline for (tested_configs) |config| {
+        const List = lib.lists.List(Payload, config);
+        const impl = comptime config.implementation.double_linked;
+        var list: List = if (impl == .sentinel_terminated)
+            undefined
+        else
+            .{};
+        list.init(); // redundant if .{} initialization is done above
+        if (comptime config.ownership_tracking == .custom)
+            list.setOwnershipToken(1);
+
+        try verifyConsistency(List, &list);
+
+        const Node = List.Node;
+        var nodes: [2]Node = undefined;
+
+        list.insert(.first, &nodes[0]); // same as list.insertFirst(&nodes[0])
+        try std.testing.expectEqual(&nodes[0], list.first());
+        try std.testing.expectEqual(&nodes[0], list.last());
+        try std.testing.expectEqual(null, list.next(&nodes[0]));
+        try std.testing.expectEqual(null, list.prev(&nodes[0]));
+
+        try verifyConsistency(List, &list);
+
+        list.insertFirst(&nodes[1]); // same as list.insert(.first, &nodes[1])
+        try std.testing.expectEqual(&nodes[1], list.first());
+        try std.testing.expectEqual(null, list.prev(&nodes[1]));
+        try std.testing.expectEqual(&nodes[0], list.next(&nodes[1]));
+        try std.testing.expectEqual(&nodes[0], list.last());
+        try std.testing.expectEqual(null, list.next(&nodes[0]));
+        try std.testing.expectEqual(&nodes[1], list.prev(&nodes[0]));
+
+        try verifyConsistency(List, &list);
+
+        var i: u32 = 0;
+        var node = list.last();
+        while (node) |n| : ({
+            node = list.prev(n);
+            i += 1;
+        })
+            try std.testing.expectEqual(&nodes[i], n);
+    }
 }
